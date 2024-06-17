369f71576d96bf190c788a2cd884293e8f9681a3
balakrishnasurya <90981476+balakrishnasurya@users.noreply.github.com>
2024-06-17 12:44:10 +0530

    this is the basic version of dct stego implemented with a backend of flask and all the required pages pf ancode and decode

--- encode.py
+++ encode.py
@@ -3,6 +3,24 @@ import cv2
 import os
 
 def encode(imagepath, secret):
+    """
+    Encodes a secret message into an image using the DCT (Discrete Cosine Transform) algorithm.
+
+    Args:
+        imagepath (str): The path to the original image file.
+        secret (str): The secret message to be encoded.
+
+    Returns:
+        str: The path to the encoded image file.
+
+    Raises:
+        None
+
+    This function reads the original image file using OpenCV's `imread` function and converts it to an unchanged format. 
+    It then creates an instance of the `DCT` class and calls its `encode_image` method to encode the secret message into the image. 
+    The encoded image is saved with a prefix of "dct_" followed by the original image file name. 
+    The function returns the path to the encoded image file.
+    """
     dct = DCT()
     original_image_file = imagepath
     dct_img = cv2.imread(original_image_file, cv2.IMREAD_UNCHANGED)
